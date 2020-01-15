09f8e33024f813be0e4eca6ea9b6cd9df126063c
Hamza <errachdi.hamza@gmail.com>
2020-01-15 15:44:15 +0100

    v1.1

--- ccrawlen.py
+++ ccrawlen.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python3
-# This file is part of an external network pentest set of tools 
-# created and maintained by Laurent Gaffie.
-# email: laurent.gaffie@gmail.com
-<<<<<<< HEAD
-# Modified by @hecvs17 (Hamza) for bug bounty purposes.
-=======
-# Modified by @hecvs17 (Hamza) for bug bounties
->>>>>>> dfc239c... v1
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+#@hecsv17/Hamza
 import requests, json, argparse, re, os, multiprocessing, sqlite3, subprocess
+
+print('''
+   CCrawlEN v1.0
+        Author: @hecvs17 -Hamza-
+''')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('domain', help = 'Target domain to lookup', type = str)
@@ -32,72 +17,47 @@ cmd = "mkdir "+Target+""
 subprocess.call(cmd, shell=True)
 currentDir = os.getcwd()
 
-def DbConnect():
-    cursor = sqlite3.connect('./Results.db')
-    return cursor
-
-def CreatePentestDb():
-	if os.path.exists('./Results.db'):
-		cursor = DbConnect()
-		cursor.execute('DROP TABLE SubDomain')
-		cursor.commit()
-		cursor.execute('CREATE TABLE SubDomain (timestamp TEXT, Domain TEXT, Links Text)')
-		cursor.commit()
-		cursor.close()
-	if not os.path.exists('./Results.db'):
-		cursor = DbConnect()
-		cursor.execute('CREATE TABLE SubDomain (timestamp TEXT, Domain TEXT, Links Text)')
-		cursor.commit()
-		cursor.close()
-
-def SaveSubDomainToDb(result):
-	for k in [ 'timestamp', 'Domain', 'Links']:
-		if not k in result:
-			result[k] = ''
-	cursor = DbConnect()
-	cursor.text_factory = sqlite3.Binary
-	res = cursor.execute("SELECT COUNT(*) AS count FROM SubDomain WHERE Domain=? AND Links=?", (result['Domain'], result['Links']))
-	(count,) = res.fetchone()
-	if not count:
-		cursor.execute("INSERT INTO SubDomain VALUES(datetime('now'), ?, ?)", (result['Domain'], result['Links']))
-		cursor.commit()
-	cursor.close()
-
-def GetSubdomainStatistic(cursor):
-     res = cursor.execute("SELECT COUNT(DISTINCT UPPER(Domain)) FROM SubDomain")
-     for row in res.fetchall():
-         print('\n[+] In total {0} unique subdomains were retrieved.'.format(row[0]))
-
-def GetSubdomains(cursor):
-     res = cursor.execute("SELECT DISTINCT Domain FROM SubDomain")
-     for row in res.fetchall():
-         print('Subdomain found: {0}'.format(row[0]))
 
 name = currentDir+"/"+Target+"/"+"files-"+Target+".txt"
 def SaveTofile(url):
 	with open(name,"a+") as f:
     		f.write(url+"\n")
-notdup = currentDir+"/"+Target+"/"+"unik-urls-"+Target+".txt"
-def RemoveDuplicate():
+	
+
+notdup = currentDir+"/"+Target+"/"+"unik-"+Target+".txt"
+def RemoveDuplicate(fp,fs):
 	lines_seen = set() # holds lines already seen
-	outfile = open(notdup, "w+")
-	for line in open(name, "r"):
+	outfile = open(fp, "a+")
+	for line in open(fs, "r+"):
     		if line not in lines_seen: # not a duplicate
         		outfile.write(line)
         		lines_seen.add(line)
 	outfile.close()
+nsubs = currentDir+"/"+Target+"/"+"temp-numbers"+Target+".txt"
+number_subs = currentDir+"/"+Target+"/"+"numbers"+Target+".txt"
+def NumberOfSubs():
+	c_outfile = open(nsubs,"a+")
+	for line in open(notdup,"r"):
+		cline = line.split("/")[2]
+		c_outfile.write(cline)
+	RemoveDuplicate(number_subs,nsubs)
+	num_lines = sum(1 for line in open(number_subs))
+	print('\n[+] In total {0} unique subdomains were retrieved.'.format(num_lines))
+	for sub in open(number_subs):
+		print('Subdomain found: {0}'.format(sub))
+	
 
 juicy = currentDir+"/"+Target+"/"+"juicy-ends-"+Target+".txt"
 def FindJuicyEndpoints():
-	outfile = open(juicy,"w+")
+	outfile = open(juicy,"a+")
+	#Feel free to add your keywords here
 	for line in open(notdup, "r"):
-		#feel free to add your key words here
 		if "url" in line or "redirect" in line or "redirect_url" in line or "redirectUrl" in line or "return" in line or "return_url" in line or "return_uri" in line or "next" in line or "next_url" in line or "goto" in line or "image=" in line or "fetch" in line or "target" in line or ".sql" in line or "filename" in line or "file=" in line:
 			outfile.write(line)
 	outfile.close()
 jsfiles = currentDir+"/"+Target+"/"+"jsfiles-"+Target+".txt"
 def FindJSfiles():
-	outfile = open(jsfiles, "w+")
+	outfile = open(jsfiles, "a+")
 	lines_seen = set()
 	for line in open(notdup,"r"):
 		if ".js" in line:
@@ -115,9 +75,6 @@ def GetLinks(CdxApi, IndexNum):
 			Url = json.loads(entry)['url']
 			Domains =  re.findall(r'(?<=://)[^/|?|#]*', Url)[0]
 			SaveTofile(Url)
-			SaveSubDomainToDb({
-					'Domain': Domains
-					})
 		print("{0} Processed".format(IndexNum))
 	except:
 		pass
@@ -135,13 +92,12 @@ def GetIndexFile():
 	for proc in threads:
 		proc.get()
 
+
+
 def main():
-	CreatePentestDb()
-	cursor = DbConnect()
 	GetIndexFile()
-	GetSubdomainStatistic(cursor)
-	GetSubdomains(cursor)
-	RemoveDuplicate()
+	RemoveDuplicate(notdup,name)
+	NumberOfSubs()
 	FindJuicyEndpoints()
 	FindJSfiles()
 if __name__ == '__main__':
